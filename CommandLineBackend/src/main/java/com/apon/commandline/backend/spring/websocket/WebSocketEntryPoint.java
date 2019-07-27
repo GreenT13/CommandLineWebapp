2f951d2fe6fc1a1320309de4d1f2e5920e525c10
Rico Apon <rico.apon@hotmail.com>
2019-07-27 21:49:37 +0200

    Reworked logging

--- CommandLineBackend/src/main/java/com/apon/commandline/backend/spring/websocket/WebSocketEntryPoint.java
+++ CommandLineBackend/src/main/java/com/apon/commandline/backend/spring/websocket/WebSocketEntryPoint.java
@@ -5,46 +5,49 @@ import com.apon.commandline.backend.spring.websocket.coders.MessageEncoder;
 import com.apon.commandline.backend.spring.websocket.command.CommandInput;
 import com.apon.commandline.backend.spring.websocket.command.CommandOutput;
 import com.apon.commandline.backend.spring.websocket.command.CommandStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 
+@SuppressWarnings("unused")
 @Component
 @ServerEndpoint(value = "/webSocket", decoders = MessageDecoder.class, encoders = MessageEncoder.class)
 public class WebSocketEntryPoint {
-
+    private Logger logger = LogManager.getLogger(WebSocketEntryPoint.class);
     private Session session;
 
     @OnOpen
     public void onOpen(Session session) {
-        System.out.println("Open Connection ...");
+        logger.info("Open connection '{}'.", session.getId());
         this.session = session;
     }
 
     @OnClose
     public void onClose() {
-        System.out.println("Close Connection ...");
+        logger.info("Close connection '{}'.", session.getId());
     }
 
     @OnMessage
     public void onMessage(CommandInput commandInput) throws IOException, EncodeException {
-        sendMessage("Message received");
-        System.out.println(commandInput.commandArg);
+        logger.info("Message received '{}'.", commandInput.commandArg);
 
+        sendMessage("Message received. Going to wait for a second.");
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        sendFinalMessage("Test");
+        sendFinalMessage("Finished.");
     }
 
     @OnError
     public void onError(Throwable e) {
-        e.printStackTrace();
+        logger.error("An error occurred", e);
     }
 
     private void sendMessage(String message) throws IOException, EncodeException {
